ad698165640327ba0649c4c888e9624558970e05
Mahid <mahidmiah899.211@outlook.com>
2021-12-15 02:57:43 +0000

    Figured out how to change cell colours.

--- src/main/kotlin/Utils/MultiLineCellRenderer.java
+++ src/main/kotlin/Utils/MultiLineCellRenderer.java
@@ -16,30 +16,40 @@ public class MultiLineCellRenderer extends JTextArea implements TableCellRendere
         setEditable(false); //this line doesn't seem to be doing anything
     }
 
-    public Component getTableCellRendererComponent(JTable table, Object value,
-        boolean isSelected, boolean hasFocus, int row, int column) {
-            if (isSelected) {
-                setForeground(table.getSelectionForeground());
-                setBackground(table.getSelectionBackground());
-            }
-            else {
-                setForeground(table.getForeground());
-                setBackground(table.getBackground());
-            }
-            setFont(table.getFont());
-                if (hasFocus) {
-                    setBorder(UIManager.getBorder("Table.focusCellHighlightBorder"));
-                    if (table.isCellEditable(row, column)) {
-                        setForeground(UIManager.getColor("Table.focusCellForeground"));
-                        setBackground(UIManager.getColor("Table.focusCellBackground"));
-                    }
-                }
-            else {
-                setBorder(new EmptyBorder(1, 2, 1, 2));
-            }
-            setText((value == null) ? "" : value.toString());
-            setEditable(false); //this line doesn't seem to be doing anything
-            return this;
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+
+        Component c = new DefaultTableCellRenderer().getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        c.setForeground(Color.red);
+
+        if (isSelected) {
+            setForeground(table.getSelectionForeground());
+            setBackground(table.getSelectionBackground());
         }
+        else {
+            setForeground(table.getForeground());
+            setBackground(table.getBackground());
+        }
+        setFont(table.getFont());
+        if (hasFocus) {
+            setBorder(UIManager.getBorder("Table.focusCellHighlightBorder"));
+            if (table.isCellEditable(row, column)) {
+                setForeground(UIManager.getColor("Table.focusCellForeground"));
+                setBackground(UIManager.getColor("Table.focusCellBackground"));
+            }
+        }
+        else {
+            setBorder(new EmptyBorder(1, 2, 1, 2));
+        }
+
+        if (value != "") {
+            setBackground(Color.orange);
+        } else {
+            setBackground(Color.yellow);
+        }
+
+        setText((value == null) ? "" : value.toString());
+        setEditable(false); //this line doesn't seem to be doing anything
+        return this;
+    }
 }
 
